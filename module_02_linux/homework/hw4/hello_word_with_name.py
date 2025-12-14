745abc1433cd40230219fc6e038bb245ed6ce9e6
vboxuser <9631633951@my.lerna.uz>
2025-12-14 17:39:16 +0000

    task_4

--- module_02_linux/homework/hw4/hello_word_with_name.py
+++ module_02_linux/homework/hw4/hello_word_with_name.py
@@ -6,15 +6,34 @@
 
 /hello-world/Саша  →  Привет, Саша. Хорошей субботы!
 """
-
+from datetime import datetime
 from flask import Flask
 
 app = Flask(__name__)
 
+WEEKDAYS_RU: tuple[str, ...] = (
+    'понедельника',
+    'вторника',
+    'среды',
+    'четверга',
+    'пятницы',
+    'субботы',
+    'воскресенья',
+)
 
-@app.route('/hello-world/...')
-def hello_world():
-    ...
+@app.route('/hello-world/<name>')
+def hello_world(name: str) -> str:
+    """
+        Endpoint приветствует пользователя по имени и желает хорошего дня недели
+        на русском языке в родительном падеже.
+
+        :param name: имя пользователя из URL
+        :return: приветственное сообщение
+        """
+    weekday_index: int = datetime.today().weekday()
+    weekday_name: str = WEEKDAYS_RU[weekday_index]
+
+    return f'Привет, {name}. Хорошего {weekday_name}!'
 
 
 if __name__ == '__main__':
