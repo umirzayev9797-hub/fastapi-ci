9294b83d90dbb15f3bb2f7f773a967e1b229f40e
vboxuser <9631633951@my.lerna.uz>
2025-12-22 07:19:17 +0000

    tasks_4

--- module_04_flask/homework/hw4/uptime.py
+++ module_04_flask/homework/hw4/uptime.py
@@ -6,14 +6,27 @@
 """
 
 from flask import Flask
+import subprocess
+import shlex
 
 app = Flask(__name__)
 
 
 @app.route("/uptime", methods=['GET'])
 def uptime() -> str:
-    ...
+    command_str = "uptime -p"
+    command = shlex.split(command_str)
+
+    result = subprocess.run(
+        command,
+        capture_output=True,
+        text=True
+    )
+
+    uptime_value = result.stdout.strip()
+
+    return f"Current uptime is {uptime_value}"
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host = "0.0.0.0")
