077a92b9ba220ccdf9b6c5e723dc5d5c2479e77d
System <it@skillbox.ru>
2026-01-08 19:07:32 +0300

    Add lesson

---
+++ module_07_logging_part_2/homework/hw3_level_file_handler/logger_helper.py
@@ -0,0 +1,12 @@
+import logging
+import sys
+
+
+class LevelFileHandler(logging.Handler):
+    ...
+
+
+def get_logger(name):
+    ...
+    return logger
+
---
+++ module_07_logging_part_2/homework/hw8_http_handler/server.py
@@ -0,0 +1,26 @@
+import json
+from flask import Flask, request
+
+
+app = Flask(__name__)
+
+
+@app.route('/log', methods=['POST'])
+def log():
+    """
+    Записываем полученные логи которые пришли к нам на сервер
+    return: текстовое сообщение об успешной записи, статус код успешной работы
+
+    """
+    ...
+
+
+@app.route('/logs', methods=['GET'])
+def logs():
+    """
+    Рендерим список полученных логов
+    return: список логов обернутый в тег HTML <pre></pre>
+    """
+    ...
+
+# TODO запустить сервер
