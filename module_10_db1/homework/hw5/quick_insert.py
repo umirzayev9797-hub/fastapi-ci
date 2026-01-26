cf53856a137fa013a8b7435687e90c542118f4ce
System <it@skillbox.ru>
2026-01-26 21:03:03 +0300

    Add lesson

---
+++ module_10_db1/homework/hw5/quick_insert.py
@@ -0,0 +1,19 @@
+from typing import Union, List
+
+Number = Union[int, float, complex]
+
+
+def find_insert_position(array: List[Number], number: Number) -> int:
+    ...
+
+
+if __name__ == '__main__':
+    A: List[Number] = [1, 2, 3, 3, 3, 5]
+    x: Number = 4
+    insert_position: int = find_insert_position(A, x)
+    assert insert_position == 5
+
+    A: List[Number] = [1, 2, 3, 3, 3, 5]
+    x: Number = 4
+    A.insert(insert_position, x)
+    assert A == sorted(A)
