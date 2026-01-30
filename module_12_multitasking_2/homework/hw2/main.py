cb23f80e253dbc449e03350dde562442bbe0f4c8
System <it@skillbox.ru>
2026-01-30 19:57:03 +0300

    Add lesson

---
+++ module_12_multitasking_2/homework/hw2/main.py
@@ -0,0 +1,10 @@
+def process_count(username: str) -> int:
+    # количество процессов, запущенных из-под
+    # текущего пользователя username
+    pass
+
+
+def total_memory_usage(root_pid: int) -> float:
+    # суммарное потребление памяти древа процессов
+    # с корнем root_pid в процентах
+    pass
---
+++ module_12_multitasking_2/homework/hw3/main.py
@@ -0,0 +1,30 @@
+from threading import Semaphore, Thread
+import time
+
+sem: Semaphore = Semaphore()
+
+
+def fun1():
+    while True:
+        sem.acquire()
+        print(1)
+        sem.release()
+        time.sleep(0.25)
+
+
+def fun2():
+    while True:
+        sem.acquire()
+        print(2)
+        sem.release()
+        time.sleep(0.25)
+
+
+t1: Thread = Thread(target=fun1)
+t2: Thread = Thread(target=fun2)
+try:
+    t1.start()
+    t2.start()
+except KeyboardInterrupt:
+    print('\nReceived keyboard interrupt, quitting threads.')
+    exit(1)
