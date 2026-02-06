3ba00c3394bf49e09e87af1ad60a3c8d3ab7d8f3
System <it@skillbox.ru>
2026-02-06 10:33:11 +0300

    Add lesson

---
+++ module_22_celery/homework/config.py
@@ -0,0 +1,15 @@
+"""
+В этом файле будут секретные данные
+
+Для создания почтового сервиса воспользуйтесь следующими инструкциями
+
+- Yandex: https://yandex.ru/support/mail/mail-clients/others.html
+- Google: https://support.google.com/mail/answer/7126229?visit_id=638290915972666565-928115075
+"""
+
+# https://yandex.ru/support/mail/mail-clients/others.html
+
+SMTP_USER = "ПОЧТА ОТПРАВИТЕЛЯ"
+SMTP_HOST = "smtp.yandex.com"
+SMTP_PASSWORD = "ПАРОЛЬ ОТ ПОЧТЫ ОТПРАВИТЕЛЯ / СПЕЦИАЛЬНЫЙ ТОКЕН ПРИЛОЖЕНИЯ"
+SMTP_PORT = 587
